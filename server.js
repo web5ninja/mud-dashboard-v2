85f8118dad9611de5294c5cdba95257927df1771
SamBean <anyasset.x@protonmail.com>
2026-04-04 19:21:48 -0700

    Delete server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080;
-app.use(express.static(__dirname));
-app.listen(PORT, () => console.log('$MUD Treasury running on port ' + PORT));
